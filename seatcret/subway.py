66cd1ca2749b9f15076e6f58f38eb300fbc6fb02
Seo Jihyeok <limeburst@icloud.com>
2020-06-03 00:59:30 +0900

    Seoul subway client refactoring
    * Support sample API key
    * Better error handling

--- seatcret/subway.py
+++ seatcret/subway.py
@@ -1,9 +1,10 @@
-from typing import List, Optional
+from typing import List
 
 import requests
 
 
-SEOUL_REALTIME_LOCATION_API_URL = 'http://swopenAPI.seoul.go.kr/api/subway/{api_key}/{format}/realtimePosition/0/1000/{subway_name}'
+SAMPLE_API_KEY = 'sample'
+SEOUL_REALTIME_LOCATION_API_URL = 'http://swopenAPI.seoul.go.kr/api/subway/{api_key}/{format}/realtimePosition/{start_index}/{end_index}/{subway_name}'
 
 SUBWAY_ID_NAMES = {
     '1001': '1호선',
@@ -26,18 +27,36 @@ SUBWAY_ID_NAMES = {
 
 
 class SeoulSubway:
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str = SAMPLE_API_KEY):
         self.api_key = api_key
 
+    def get_realtime_location(self, subway_name: str,
+                              start_index: int = 0, end_index: int = 1000) -> List:
+        if self.api_key == SAMPLE_API_KEY:
+            start_index = 0
+            end_index = 50
 
-    def get_realtime_location(self, subway_name: str) -> Optional[List]:
-        r = requests.get(SEOUL_REALTIME_LOCATION_API_URL.format(
+        url = SEOUL_REALTIME_LOCATION_API_URL.format(
             api_key=self.api_key,
             format='json',
-            subway_name=subway_name
-        ))
-        try:
-            decoded = r.json()
-        except ValueError:
-            return None
-        return decoded.get('realtimePositionList', None)
+            subway_name=subway_name,
+            start_index=start_index,
+            end_index=end_index,
+        )
+        r = requests.get(url)
+        r.raise_for_status()
+
+        d = r.json()
+        if 'realtimePositionList' not in d:
+            """
+            code: INFO-200
+            message: 해당하는 데이터가 없습니다.
+
+            해당 지하철 노선 운행이 종료되었을 때 반환됨.
+            """
+            if d['status'] == 500 and d['code'] == 'INFO-200':
+                return []
+
+            raise Exception(f"{d['code']}: {d['message']}")
+        else:
+            return d['realtimePositionList']
